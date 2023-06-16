9f6f651cd41ae049ffc30c83cf528b77e99c8560
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2023-06-16 22:51:15 +0300

    deleted file not needed

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Dashboard from "./views/dashbord/Dashboard";
 import Login from "./views/auth_forms/Login";
 import Signup from "./views/auth_forms/Signup";
-import { CssBaseline } from "@mui/material";
+
 function App() {
   return (
     <BrowserRouter>
-      {/* <CssBaseline /> */}
+     
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/login/*" element={<Login />} />
